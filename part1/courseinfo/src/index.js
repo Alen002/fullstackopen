53dc7aa0dc5bdb7bb3c9b755216c3881ee270642
Alen <alechkr@gmail.com>
2020-06-30 14:12:25 +0200

    adds part1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -33,26 +33,27 @@ const Total = (props) => (
 // Root Component App
 const App = () => {
 const course = 'Half Stack application development';
-const part1 = {
-  name: 'Fundamentals of React',
-  exercises: 10
-};
-const part2 = {
-  name: 'Using props to pass data',
-  exercises: 7
-};
-const part3 = {
-  name: 'State of a component',
-  exercises: 14
-};
-
+const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+];
 return (
   <>
     <Header course = {course} />
-    <Part part = {part1.name} exercises = {part1.exercises} />
-    <Part part = {part2.name} exercises = {part2.exercises} />
-    <Part part = {part3.name} exercises = {part3.exercises} />
-    <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises} />
+    <Part part = {parts[0].name} exercises = {parts[0].exercises} />
+    <Part part = {parts[1].name} exercises = {parts[1].exercises} />
+    <Part part = {parts[2].name} exercises = {parts[2].exercises} />
+    <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises} />
   </>
 );
 };
