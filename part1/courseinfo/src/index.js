48cfc041ab988abcad8124437cefd0062c85880b
Alen <alechkr@gmail.com>
2020-06-30 14:54:28 +0200

    adds part1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -25,35 +25,38 @@ const Part = (props) => (
   );
 
 
-// Total Component
+// Total
 const Total = (props) => (
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 );
 
 // Root Component App
 const App = () => {
-const course = 'Half Stack application development';
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-];
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+};
 return (
   <>
-    <Header course = {course} />
-    <Part part = {parts[0].name} exercises = {parts[0].exercises} />
-    <Part part = {parts[1].name} exercises = {parts[1].exercises} />
-    <Part part = {parts[2].name} exercises = {parts[2].exercises} />
-    <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises} />
+    <Header course = {course.name} />
+    <Part part = {course.parts[0].name} exercises = {course.parts[0].exercises} />
+    <Part part = {course.parts[1].name} exercises = {course.parts[1].exercises} />
+    <Part part = {course.parts[2].name} exercises = {course.parts[2].exercises} />
+    <Total exercises1 = {course.parts[0].exercises} exercises2 = {course.parts[1].exercises} exercises3 = {course.parts[2].exercises} />
+    
   </>
 );
 };
