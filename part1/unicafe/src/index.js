cd9d81008be2c3267e1d88641b228a565639393d
Alen <alechkr@gmail.com>
2020-07-04 20:01:01 +0200

    part1.9 unicafe step4 - display statistics only once feedback has been gathered

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,7 +17,6 @@ const Statistics = ({stateGood, stateNeutral, stateBad}) => {
   const resultsPositive = (stateGood/(stateGood + stateNeutral + stateBad)).toFixed(2)*100 + '%'
   return (
     <>
-    <h2>Statistics</h2>
       Good: {stateGood}<br/>
       Neutral: {stateNeutral}<br/>
       Bad: {stateBad}<br/>
@@ -34,15 +33,23 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  let partial;
+
+  // Show Statistics component if state > 0
+  if (good || neutral || bad > 0) {
+    partial = <Statistics stateGood={good} stateNeutral={neutral} stateBad={bad} />
+  };
   
   return (
     <div>
       <h2>Give Feedback</h2>
       <UserFeedback criterion={'good'} state={() => setGood(good + 1)}/>  
       <UserFeedback criterion={'neutral'} state={() => setNeutral(neutral + 1)}/>  
-      <UserFeedback criterion={'bad'} state={() => setBad(bad + 1)}/>  
-
-      <Statistics stateGood={good} stateNeutral={neutral} stateBad={bad} />
+      <UserFeedback criterion={'bad'} state={() => setBad(bad + 1)}/>
+      <br/>
+      
+      <h2>Statistics</h2>
+      {partial}
     </div>
   );
 };
