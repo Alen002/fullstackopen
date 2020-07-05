f92e4fc495b10a2586c242ddef4b75d210c0048d
Alen <alechkr@gmail.com>
2020-07-05 15:53:29 +0200

    part1.14 anecdotes step3 - display anecdote with largest number of votes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -7,20 +7,25 @@ const App = (props) => {
   
   const addArray = () => {
   const copy = [...points];
-    
-    // vote is based on the anectodes in the array
+
     copy[selected] += 1;
     setVote(copy);
 
     console.log(copy);    
   };
 
+  const indexOfMaxValue = points.indexOf(Math.max(...points));
+
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       {anecdotes[selected]}<br />
       has {points[selected]} votes<br />
       <button onClick={() => setSelected(Math.floor(Math.random() * 6))}>next anecdotes</button>
       <button onClick={addArray}>Vote</button>
+      <br />
+      <h2>Anecdote With Most Votes</h2>
+      <p>{anecdotes[indexOfMaxValue]}</p>
     </div>
   );
 };
