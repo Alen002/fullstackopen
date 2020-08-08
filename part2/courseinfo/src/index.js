7d87eacd4ea496617e4b71d7e38821608fccac9b
Alen <alechkr@gmail.com>
2020-08-08 18:31:17 +0200

    part2.2-2.3 course information step 7 and 8

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -8,7 +8,12 @@ const Course = (props) => {
       <>
         {courses.map((value, i) =>
           <p key={i}>{value.name} {value.exercises}</p>
-        )}   
+        )} 
+
+        <h4>
+          Total of {courses.reduce((sum, end) => sum + end.exercises, 0)} exercises
+        </h4>
+    
       </>
     );  
 };
